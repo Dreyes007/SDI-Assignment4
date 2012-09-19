2cd57e3e9927e784c35ce50a06ebce604c871a48
Daniel Reyes <Kurlzs@Daniels-MacBook-Pro.local>
2012-09-19 03:07:48 -0400

    Added Days difference between 2 dates function

--- Reyes_daniel_assignment4.js
+++ Reyes_daniel_assignment4.js
@@ -22,20 +22,21 @@ var myLibrary = function(){
     	number.toFixed(2)
     	return console.log(number.toFixed(2));
     };
-	
-	
-	
-	
+
+			
 	//Phone # Function
 	var checkPhone = function (tryNumber) {
+	
         var phoneNumber = tryNumber;
-        var pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; 
+        var pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+         
         if (pattern.test(phoneNumber)) { 
             var validPhoneNumber = phoneNumber.replace(pattern, "($1) $2-$3");
             return console.log("This phone number is valid!"); 
         } else {
             return console.log("Sorry! this phone number is invalid!"); 
         };
+        return phoneNumber;
     };
     
     //Email check function
@@ -50,8 +51,9 @@ var myLibrary = function(){
         console.log("Sorry! This is an invalid email address!");
         
         };
-    };
-    
+        return emailAddress;
+    };	
+        
 		
 	//Name function
 	var areYouDaniel = function(val){
@@ -72,13 +74,23 @@ var myLibrary = function(){
 		};
 	};
 	
+	//Days Difference
+	var daysLeft = function (checkDate) {
+    	var vacationDays = checkDate;
+    	today = new Date(2012, 8, 20);
+    	var one_day = 1000 * 60 * 60 * 24;
+    	return (Math.ceil((vacationDays.getTime() - today.getTime()) / (one_day)) + " days untill Thanks Giving breaks starts! ");
+    };
+
+	
 	return {
 		"checkNumeric": checkNumeric,
 		"areYouDaniel": areYouDaniel,
 		"checkURL": checkURL,
 		"checkPhone": checkPhone,
 		"emailCheck": emailCheck,
-		"decNumber": decNumber
+		"decNumber": decNumber,		
+		"daysLeft": daysLeft
 	};	
 
 };
@@ -92,5 +104,6 @@ console.log(newLib.checkURL("Fullsail.com"));
 console.log(newLib.checkPhone("321-745-7444"));
 console.log(newLib.emailCheck("Dreyes007@gmail.com"));
 console.log(newLib.decNumber(2.1));
+console.log(newLib.daysLeft(new Date(2012, 10, 22)));
 
 
