a9e253b1a2a53b765bdd07cab9b312e523525961
Daniel Reyes <Kurlzs@Daniels-MacBook-Pro.local>
2012-09-18 22:23:14 -0400

    Added Phone # function

--- Reyes_daniel_assignment4.js
+++ Reyes_daniel_assignment4.js
@@ -1,8 +1,68 @@
 //Daniel Reyes
 //SDI-1209
-//Assignment 4
+//Project 4
 //Library of Functions
 
 var myLibrary = function(){
+
+	//Check numeric function
+	var checkNumeric = function(val){
+		
+		if(isNaN(val)){
+			return false;
+		} else {
+			return true;
+			
+		};
+			
+	};
+	//Phone # Function
+	var checkPhone = function (tryNumber) {
+        var phoneNumber = tryNumber;
+        var pattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; 
+        if (pattern.test(phoneNumber)) { 
+            var validPhoneNumber = phoneNumber.replace(pattern, "($1) $2-$3");
+            return console.log("This phone number is valid!"); 
+        } else {
+            return console.log("Sorry! this phone number is invalid!"); 
+        }
+    }
+		
+	//Name function
+	var areYouDaniel = function(val){
+		if (val === "Daniel") {
+			return true;
+		} else {
+			return false;
+		};
+	};
 	
+	//Check URL function
+	var checkURL = function(val){
+		var first = val.substring(0,val.indexOf(":"));
+		if((first === "http") || (first === "https")){
+			return true;
+		} else {
+			return false;
+		};
+	};
+	
+	return {
+		"checkNumeric": checkNumeric,
+		"areYouDaniel": areYouDaniel,
+		"checkURL": checkURL,
+		"checkPhone": checkPhone
+	};	
+
 };
+
+
+var newLib = new myLibrary();
+
+console.log(newLib.checkNumeric(15));
+console.log(newLib.areYouDaniel("Daniel"));
+console.log(newLib.checkURL("Fullsail.com"));
+console.log(newLib.checkPhone("321-745-7444"));
+
+
+
